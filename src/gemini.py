de8f2b14444f80e19899374917290c3b4bf609c7
Thomas Tramley <thomas@caddieservice.co>
2021-03-11 16:12:35 -0400

    Prune cache so we don't keep cache for really old pages

--- src/gemini.py
+++ src/gemini.py
@@ -14,6 +14,7 @@ import textwrap
 import urllib.parse
 import pyotherside
 import pickle
+import time
 
 storage_dir = "/home/phablet/.local/share/gem.aaron"
 
@@ -27,6 +28,8 @@ class Gemini:
         # Load future
         future_data = self.read_file("future.dat")
         self.future = future_data if future_data != None else []
+        # cache_limit prevents all pages from being cached
+        self.cache_limit = 5
 
     def read_file(self, filename):
         filepath = "{}/{}".format(storage_dir, filename)
@@ -188,7 +191,7 @@ class Gemini:
         self.history.append(url)
 
         # Reset the future.
-        self.clean_cache(self.future)
+        self.remove_from_cache(self.future)
         self.future = []
         pyotherside.send('hideForward')
 
@@ -202,20 +205,34 @@ class Gemini:
             self.load("gemini://gemini.circumlunar.space/servers/")
 
     def cache_page(self, url, content):
-        self.page_cache[url] = content
+        self.page_cache[url] = {
+            "content": content,
+            "timestamp": time.time()
+        }
 
-    def clean_cache(self, url_list):
-        assert type(url_list) is list
+    def prune_cache(self):
+        # Find urls that are too old to be cached
+        old_history = self.history[self.cache_limit:]
+        old_future = self.future[self.cache_limit:]
+        old_urls = old_history.extend(old_future)
+
+        if old_urls:
+            self.remove_from_cache(old_urls)
+
+    def remove_from_cache(self, url_list):
         # Removes cached values for the provided list of urls
         for url in url_list:
             if url in self.page_cache:
                 del self.page_cache[url]
 
     def load(self, url, using_cache = False):
+        if len(self.history) > self.cache_limit or len(self.future) > self.cache_limit:
+            self.prune_cache()
+        self.prune_cache()
         pyotherside.send('loading', url)
 
         if using_cache and url in self.page_cache:
-            return pyotherside.send('onLoad', self.page_cache[url])
+            return pyotherside.send('onLoad', self.page_cache[url]['content'])
 
         try:
             gemsite = self.get_site(url)
